b85b811f2a86c4bcdeb3d07b4ed3968f78322a4b
YioJoe <yioyiomain@gmail.com>
2025-02-01 14:57:26 -0500

    First Commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
+
+const Profile = () => {
+  const [changeColor, setChangeColor] = useState(false)
+
+  const handlePress = () => {
+    setChangeColor(!changeColor)
+  }
+
+  return (
+    <View className={changeColor ? 'bg-slate-400' : 'bg-slate-100'} style={{marginTop: 30}}>
+      <Text style={changeColor && styles.changeTextColor} className='text-orange-600 text-2xl mb-10'>Profile</Text>
+      <Pressable onPress={handlePress}>
+        <Text className='bg-slate-400 text-red-500 font-semibold p-5 rounded-lg'>Change Color</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create(
+  {
+    changeTextColor: {
+      color: 'red'
+    },
+  }
+)
+
+export default Profile
---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import "../global.css";
+import { Stack } from 'expo-router';
+
+const HomeLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }}/>
+        <Stack.Screen name="profile" options={{ headerShown: false}}/>
+    </Stack>
+  )
+}
+
+export default HomeLayout
