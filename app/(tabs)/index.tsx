4748bc08ce0e865112ccf7265d075409faddea8e
YioJoe <yioyiomain@gmail.com>
2025-02-19 22:19:07 -0500

    cambios menores

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import { Link } from 'expo-router'
 import React from 'react'
 
 import { bgRed } from '../../components/tokens'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import IndexMenu from '../../modules/tabs/IndexMenu'
 
 const Index = () => {
   return (
-    <View>
-      <Text className='text-4xl text-red-600'>Index</Text>
-      <Link href='/profile/details' >
-        <Text className={`bg-primary border border-secondary-100 rounded p-4 text-3xl`}>
-            Profile
-        </Text>
-      </Link>
-    </View>
+    <SafeAreaView>
+      <View>
+        <StatusBar barStyle="dark-content" backgroundColor="white" />
+        <IndexMenu/>
+        <Text className='text-4xl text-red-600'>Index</Text>
+        <Link href='/profile/details' >
+          <Text className={`bg-primary border border-secondary-100 rounded p-4 text-3xl`}>
+              Profile
+          </Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   )
 }
 
