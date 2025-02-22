f4f14dbed8ec7533eaa56284eb6fab7c79e3f107
YioJoe <yioyiomain@gmail.com>
2025-02-22 11:13:10 -0500

    añadiendo vista menu diario

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 import { bgRed } from '../../components/tokens'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import IndexMenu from '../../modules/tabs/IndexMenu'
+
 
 const Index = () => {
   return (
     <SafeAreaView>
       <View>
         <StatusBar barStyle="dark-content" backgroundColor="white" />
-        <IndexMenu/>
+        <DailyMenu/>
         <Text className='text-4xl text-red-600'>Index</Text>
         <Link href='/profile/details' >
           <Text className={`bg-primary border border-secondary-100 rounded p-4 text-3xl`}>
