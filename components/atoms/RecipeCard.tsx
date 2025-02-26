992b982c68e9a152c4b856398bc322509deeb4aa
YioJoe <yioyiomain@gmail.com>
2025-02-26 11:28:59 -0500

    añadiendo recipecard y sus imagenes

--- components/atoms/RecipeCard.tsx
+++ components/atoms/RecipeCard.tsx
@@ -1,5 +1,7 @@
-import { Image, View, Text, FlatList } from 'react-native'
+import { Image, View, Text, FlatList, Pressable } from 'react-native'
 import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 import { RecipeList } from '../../data/RecipesData'
 
 const RecipeCard = () => {
@@ -9,15 +11,21 @@ const RecipeCard = () => {
             data={RecipeList}
             keyExtractor={(item) => item.id} 
             renderItem={({ item }) => 
-            <View 
+            <Pressable
+                onPress={() => alert("hi")} 
                 className='bg-white shadow-black shadow-lg rounded-2xl my-4 mx-5 items-center py-6 px-2' 
                 style={{shadowOpacity: 0.1, shadowRadius: 7}}>
                 <Image source={item.image} className='w-[150px] h-[150px] rounded-2xl' resizeMode='cover' />
                 <Text>{item.name}</Text>
-                <View>
-                <Text>{item.time}</Text>
+                <View className='mt-2' style={{flexDirection: "row"}}>
+                    <Text>{item.time}</Text>
+                    <Text> | </Text>
+                    <View className='justify-center items-center' style={{flexDirection: "row"}}>
+                        <Text className='mr-1'>{item.rating}</Text>
+                        <FontAwesome name='star' size={16} color={"#ff7514"} />
+                    </View>
                 </View>
-            </View>}
+            </Pressable>}
             scrollEnabled={false}
             numColumns={2}
             columnWrapperStyle={{
