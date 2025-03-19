f78405c1841be4fffbce402f32b0fc3a3de4b87b
YioJoe <yioyiomain@gmail.com>
2025-03-18 21:39:25 -0500

    url

--- server/token.ts
+++ server/token.ts
@@ -1,4 +1,4 @@
-const apiFast = "https://sensitivity-obj-mississippi-rugby.trycloudflare.com/";
+const apiFast = "https://violin-hate-distinguished-rv.trycloudflare.com/";
 
 export const authToken = {
   login: apiFast + "api/auth/signin",
