7f162d89b2b686ee0147f6213dd79aa5ed74df58
YioJoe <yioyiomain@gmail.com>
2025-03-28 19:34:47 -0500

    Feature: Connection to Firebase notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,20 @@ import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
 import { ProtectedRoute } from '../context/ProtectedRoute';
 
+import { usePushNotifications } from '../hooks/usePushNotifications'; 
+import { useEffect } from 'react';
+
 const HomeLayout = () => {
   
+  const { expoPushToken, notification } = usePushNotifications()
+
+  useEffect(() => {
+    console.log("expoPushToken", expoPushToken);
+    console.log("notification", notification);
+    
+    
+  })
+
   return (
     <AuthProvider>
       <Stack>
